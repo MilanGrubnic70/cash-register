70abb33db94f186c7f78a52f91478b7c8413af9f
Milan Grubnić <MilanG@aol.com>
2016-01-22 20:31:38 -0600

    added add function

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 var init = function() {
 
     var cashRegister = {
-        total: 0
+        total: 0,
+        add: function(itemCost) {
+            this.total += itemCost;
+        }
     };
 
-};
+    cashRegister.add(0.98);
+    cashRegister.add(1.23);
+    cashRegister.add(4.99);
+    cashRegister.add(0.45);
+
+
+
+    console.log("Your bill is: " + cashRegister.total + ".");
+
+}; // end of init
 
 $(document).ready(init);
